2608c66c937cd22168485bac380f0564edd3c6af
SAYEM5522 <md1040582@gmail.com>
2021-08-22 20:04:33 +0600

    BottomTab Add

--- App.js
+++ App.js
@@ -7,16 +7,55 @@ import HomeScreen from './Screen/HomeScreen';
 import ShopScreen from './Screen/ShopScreen';
 import ChatScreen from './Screen/ChatScreen';
 import ProfileScreen from './Screen/ProfileScreen';
-
+import { Entypo } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
+import HomeHeader from './Components/HomeHeader';
 export default function App() {
   const Tab = createBottomTabNavigator();
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Shop" component={ShopScreen} />
-        <Tab.Screen name="Chat" component={ChatScreen} />
-        <Tab.Screen name="Profile" component={ProfileScreen} />
+      <Tab.Navigator 
+      
+        screenOptions= {{
+       tabBarShowLabel:false
+        }}
+      >
+        <Tab.Screen
+        
+         options={{
+          tabBarIcon: ({ focused }) => (
+            <Entypo name="home" size={30} color="black" color={focused?"black":'gray'} />
+          ),
+          headerShown:false,
+            
+        }
+      }
+        name="Home" component={HomeScreen} />
+        <Tab.Screen
+         options={{
+          tabBarIcon: ({ focused }) => (
+            <FontAwesome5 name="shopping-bag" size={30} color="black" />
+          ), 
+
+        }}
+        name="Shop" component={ShopScreen} />
+        <Tab.Screen
+         options={{
+          tabBarIcon: ({ focused }) => (
+            <Ionicons name="md-chatbubble-sharp" size={30} color="black" />
+          ), 
+
+        }}
+        name="Chat" component={ChatScreen} />
+        <Tab.Screen
+         options={{
+          tabBarIcon: ({ focused }) => (
+            <Entypo name="emoji-neutral" size={30} color="black" />
+          ), 
+
+        }}
+        name="Profile" component={ProfileScreen} />
 
 
       </Tab.Navigator>
---
+++ Components/HomeHeader.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const HomeHeader = () => {
+  return (
+    <View>
+      <Text>headder</Text>
+    </View>
+  )
+}
+
+export default HomeHeader
+
+const styles = StyleSheet.create({})
