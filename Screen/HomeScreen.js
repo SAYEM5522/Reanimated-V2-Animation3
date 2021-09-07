3176e7c9ca29563fe11b1d6e3d2ce6c6ed31d3b9
SAYEM5522 <md1040582@gmail.com>
2021-09-07 22:52:17 +0600

    header add

--- Screen/HomeScreen.js
+++ Screen/HomeScreen.js
@@ -1,12 +1,18 @@
 import { LinearGradient } from 'expo-linear-gradient'
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StatusBar, StyleSheet, Text, View } from 'react-native'
 import StackImage from '../Components/StackImage'
-
+import { FontAwesome5 } from '@expo/vector-icons';
 const HomeScreen = () => {
   return (
     <View style={{ flex: 1 }}>
-      <View style={styles.container}></View>
+      <StatusBar/>
+      <View style={styles.container}>
+        <View style={styles.view1}/>
+        <View style={styles.view2}/>
+        <Text style={styles.HeaderText}>Magaz</Text>
+        <FontAwesome5 name="minus-square" style={styles.HeaderIcon} size={30} color="white" />
+      </View>
       <LinearGradient
         colors={['rgba(0,0,0,0.98)','rgba(250,250,250,250)', ]}
         style={styles.background}/>
@@ -30,6 +36,35 @@ const styles = StyleSheet.create({
 },
 container:{
   height:300,
-  backgroundColor:'black'
+  backgroundColor:'black',
+  
+},
+HeaderText:{
+  left:65,
+  fontSize:35,
+  color:'white',
+  fontWeight:'bold',
+  top:-30
+},
+view1:{
+  width:23,
+  height:23,
+  borderRadius:12,
+  backgroundColor:"#d94545",
+  top:26,
+  left:30
+},
+view2:{
+  width:23,
+  top:14,
+  height:23,
+  borderRadius:12,
+  backgroundColor:"#5cedd0",
+  left:20
+},
+HeaderIcon:{
+  left:360,
+  top:-60,
+  
 }
 })
